eea27dab93f7bc1960a56a9dbc8227bdb7a25573
Kirill1one <kirill2002ku@gmail.com>
2024-07-16 17:00:18 +0300

    New project

--- src/content_option.js
+++ src/content_option.js
@@ -1,107 +1,118 @@
-const logotext = "Kirill";
+const logotext = 'Kirill'
 const meta = {
-    title: "Kirill",
-    description: "I’m Kirill Frontend developer",
-};
+	title: 'Kirill',
+	description: 'I’m Kirill Frontend developer',
+}
 
 const introdata = {
-    title: "I’m Kirill",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites"
-    },
-    description: "Self-motivated and passionate front-end developer with a keen interest.",
-    your_img_url: "https://avatars.githubusercontent.com/u/104112337?v=4",
-};  
+	title: 'I’m Kirill',
+	animated: {
+		first: 'I love coding',
+		second: 'I code cool websites',
+	},
+	description:
+		'Self-motivated and passionate front-end developer with a keen interest.',
+	your_img_url: 'https://avatars.githubusercontent.com/u/104112337?v=4',
+}
 
 const dataabout = {
-    title: "Abit about me",
-    aboutme: "I'm a frontend developer. My ability to work with JavaScript, React, Redux, Node JS technologies. I am also responsible, goal-oriented and strive for constant development and acquisition of new skills.",
-};
-const worktimeline = [{
-        jobtitle: "Freelance",
-        where: "Frontend developer",
-        date: "November 2022 - June 2023",
-    },
-    {
-        jobtitle: "BIG BEAR VANS",
-        where: "Frontend developer",
-        date: "June 2023 - September 2023s",
-    },
-    {
-        jobtitle: "Freelance",
-        where: "Frontend developer",
-        date: "October 2023 – January 2024",
-    },
-];
+	title: 'Abit about me',
+	aboutme:
+		"I'm a frontend developer. My ability to work with JavaScript, React, Redux, Node JS technologies. I am also responsible, goal-oriented and strive for constant development and acquisition of new skills.",
+}
+const worktimeline = [
+	{
+		jobtitle: 'Freelance',
+		where: 'Frontend developer',
+		date: 'November 2022 - June 2023',
+	},
+	{
+		jobtitle: 'BIG BEAR VANS',
+		where: 'Frontend developer',
+		date: 'June 2023 - September 2023s',
+	},
+	{
+		jobtitle: 'Freelance',
+		where: 'Frontend developer',
+		date: 'October 2023 – January 2024',
+	},
+]
 
-const skills = [ 
-    {
-        name: "Languages and Frontend",
-        description: "HTML, CSS, JavaScript"
-    },
-    {
-        name: "Frameworks and Libraries",
-        description: "React-Bootstrap, React, Redux, Node.js"
-    },
-    {
-        name: "Tools",
-        description: "WordPress, GIT, Figma, Netlify,  Soft-UI CSS"
-    },
-    
-];
+const skills = [
+	{
+		name: 'Languages and Frontend',
+		description: 'HTML, CSS, JavaScript',
+	},
+	{
+		name: 'Frameworks and Libraries',
+		description: 'React-Bootstrap, React, Redux, Node.js',
+	},
+	{
+		name: 'Tools',
+		description: 'WordPress, GIT, Figma, Netlify,  Soft-UI CSS',
+	},
+]
 
- 
-
-const dataportfolio = [ 
-    {
-        img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/66125c500891b7923047ca9c/screenshot_2024-04-07-08-41-54-0000.webp&fit=cover&h=420&w=900",  
-        description: "Nike website",
-        skills: "HTML, Tailwind, JS, React.JS",
-        link: "https://github.com/Kirill1one/Nike-React",
-    },
-    {
-        img: "https://kirillportfolioreact.netlify.app/static/media/06.5e21f4aaed36d90944a4.jpg",
-        description: "Quiz",
-        skills: "HTML, SCSS, JS, React.JS",
-        link: "https://github.com/Kirill1one/Quiz-react",
-    },
-    {
-        img: "https://wpshapere.com/blog/wp-content/uploads/2020/11/Kelvin.jpg",
-        description: "Freelance portfolio",
-        skills: "HTML, CSS, JS",
-        link: "https://github.com/Kirill1one/Freelance-portfolio",
-    },
-     {
-        img: "https://kirillportfolioreact.netlify.app/static/media/04.b74c94c99f3e42848a87.jpg",
-        description: "Game click",
-        skills: "HTML, CSS, JS",
-        link: "https://github.com/Kirill1one/06-Sixth-project-game-",
-    },
-    {
-     img: "https://cdn.dribbble.com/users/1313402/screenshots/15902209/media/f22927f92d00f035b090ddff1bf71917.gif",
-        description: "Data submission form",
-        skills: " HTML, CSS, JS, React.JS",
-        link: "https://github.com/Kirill1one/data-submission-form",
-          
-    },
-    
-];
+const dataportfolio = [
+	{
+		img: 'https://cdn.dribbble.com/userupload/6837853/file/original-602adfe48baaa14ad8de4fe14f0e4c51.png?crop=0x0-3201x2401&resize=1600x1200',
+		description: 'Website with Modern UI/UX in React JS with Tailwind',
+		skills: 'React, Tailwind CSS, JS',
+		link: 'https://github.com/Kirill1one/BrainWave',
+	},
+	{
+		img: 'https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/66125c500891b7923047ca9c/screenshot_2024-04-07-08-41-54-0000.webp&fit=cover&h=420&w=900',
+		description: 'Nike website',
+		skills: 'HTML, Tailwind CSS, JS, React.JS',
+		link: 'https://github.com/Kirill1one/Nike-React',
+	},
+	{
+		img: 'https://kirillportfolioreact.netlify.app/static/media/06.5e21f4aaed36d90944a4.jpg',
+		description: 'Quiz',
+		skills: 'HTML, SCSS, JS, React.JS',
+		link: 'https://github.com/Kirill1one/Quiz-react',
+	},
+	{
+		img: 'https://wpshapere.com/blog/wp-content/uploads/2020/11/Kelvin.jpg',
+		description: 'Freelance portfolio',
+		skills: 'HTML, CSS, JS',
+		link: 'https://github.com/Kirill1one/Freelance-portfolio',
+	},
+	{
+		img: 'https://kirillportfolioreact.netlify.app/static/media/04.b74c94c99f3e42848a87.jpg',
+		description: 'Game click',
+		skills: 'HTML, CSS, JS',
+		link: 'https://github.com/Kirill1one/06-Sixth-project-game-',
+	},
+	{
+		img: 'https://cdn.dribbble.com/users/1313402/screenshots/15902209/media/f22927f92d00f035b090ddff1bf71917.gif',
+		description: 'Data submission form',
+		skills: ' HTML, CSS, JS, React.JS',
+		link: 'https://github.com/Kirill1one/data-submission-form',
+	},
+]
 
 const contactConfig = {
-    YOUR_EMAIL: "kirill2002ku@gmail.com",
-    YOUR_FONE: "+7 (904) 463 78-15",
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
-};
+	YOUR_EMAIL: 'kirill2002ku@gmail.com',
+	YOUR_FONE: '+7 (904) 463 78-15',
+	YOUR_SERVICE_ID: 'service_id',
+	YOUR_TEMPLATE_ID: 'template_id',
+	YOUR_USER_ID: 'user_id',
+}
 
 const socialprofils = {
-    github: "https://github.com/Kirill1one",
-    vk: "https://vk.com/kirilll_kup",
-    telegram: "https://t.me/kkiVel_1"
-};
+	github: 'https://github.com/Kirill1one',
+	vk: 'https://vk.com/kirilll_kup',
+	telegram: 'https://t.me/kkiVel_1',
+}
 export {
-    contactConfig, dataabout,
-    dataportfolio, introdata, logotext, meta, skills, socialprofils, worktimeline
-};
+	contactConfig,
+	dataabout,
+	dataportfolio,
+	introdata,
+	logotext,
+	meta,
+	skills,
+	socialprofils,
+	worktimeline,
+}
